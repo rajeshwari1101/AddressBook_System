23a7e860cc3a249d8822659dc7f4fbfebe2d79ba
rajeshwari1101 <rajeshwaripatil1101@gmail.com>
2022-02-25 18:48:29 +0530

    creating new contact

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem 
+{
+    internal class AddressBook
+    {
+
+        public Dictionary<string, Contact> contacts;
+
+        public AddressBook()
+        {
+            contacts = new Dictionary<string, Contact>();
+        }
+
+        public void CreateContact()
+        {
+            Contact tempContact = new Contact();
+            tempContact.GetUserInfo();
+            string name = tempContact.GetName();
+            if (contacts.ContainsKey(name) is false)
+            {
+                contacts.Add(name, tempContact);
+            }
+            else
+            {
+                Console.WriteLine("erorr");
+            }
+        }
+
+        public void Display()
+        {
+            foreach (string name in contacts.Keys)
+                contacts[name].Display();
+        }
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====Welcome To Address Book====");
+            AddressBook address = new AddressBook();
+            address.CreateContact();
+            address.Display();
+            Console.ReadKey();
+        }
+    }
+}
